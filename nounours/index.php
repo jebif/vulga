5a48fe02d0862e7fdf2039d89367a6d2ea048861
marsu15 <julien.fumey@gmail.com>
2020-09-20 21:50:35 +0200

    change intro text on homepage

--- nounours/index.php
+++ nounours/index.php
@@ -3,13 +3,14 @@ require("headerNounours.php");
 ?>
     <h1 class="titreApp">Les aventures de Nounours</h1>
     <!--<img src="./EcoleADN/Crotte.JPG" alt = "Crotte à analyser" class="rounded float-right pl-3">-->
-    <p>Nounours, la mascotte de notre association Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum. Cras mattis consectetur purus sit amet fermentum.</p>
+    <p>Nounours, l’ours en peluche de l’association JeBiF, est désormais une star des fêtes de la science. Pour pouvoir lui donner encore plus de visibilité JeBiF a décidé d’adapter ses aventures sous forme de jeux en ligne. Découvrez dès à présent notre premier Atelier : Les Experts BIOINFO !</p>
 
     <div class="row">
         <div class="card col-4">
             <img src="../media/nounours/experts.png" class="card-img-top">
             <h5 class="card-title"><a href="experts_accueil.php">Les experts bioinfo</a></h5>
-            <p class="card-text">Donec ullamcorper nulla non metus auctor fringilla. Etiam porta sem malesuada magna mollis euismod. Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            <p class="card-text">“Nounours a très mal au ventre ! Il a attrapé la blastouille ! Les médecins savent que cela est dû à une bactérie, mais ne savent pas laquelle, et, plus embêtant, les antibiotiques traditionnels ne fonctionnent pas sur cette maladie.</p>
+            <p card-text>Mais vous, experts en bioinformatiques que vous êtes, allez pouvoir démêler ce mystère à travers divers jeux en ligne !</p>
         </div>
     <!--</div>-->
 
