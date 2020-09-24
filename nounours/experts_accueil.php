d70f398469dc7d0f4a2952088d5d03ed9179496b
marsu15 <julien.fumey@gmail.com>
2020-09-24 18:55:24 +0200

    Change design of the home page

--- nounours/experts_accueil.php
+++ nounours/experts_accueil.php
@@ -1,24 +1,42 @@
 <?php
+$bodyclass = 'landscape';
 require("headerNounours.php");
 ?>
 
 <h1>Les experts bioinfo</h1>
 
-<div class="row">
-    <div class="col-8">
-        <audio controls autoplay src='../media/sons/intro_experts_bioinfo.mp3' type="audio/mpeg"></audio>
-        <p>Voici Nounours, notre mascotte. Il a très mal au ventre. Le médecin des peluches, le nounoursologue, est formel: il a attrapé une maladie que seule les ours en peluche peuvent attraper : <b>la blastouille</b> ! C’est une maladie bactérienne, mais de bactérie inconnue. Les médicaments que le nounoursologue lui a donné n’ont pas marché !</p>
-
-        <p>Aide nous à mener l'enquête pour identifier la bactérie responsable de la maladie de Nounours et lui trouver un médicament.</p>
+<div class="row justify-content-around">
+    <div class="col-7">
+        <!--<audio controls autoplay src='../media/sons/intro_experts_bioinfo.mp3' type="audio/mpeg"></audio>-->
+        <div class="explication">
+            <p>Voici <span class="bleuclair">Nounours</span>, c'est notre mascotte. Hier il a mangé au restaurant.</p>
+            <p>Depuis ce matin, il a <span class="bleu">très mal au ventre</span>.</p>
+            <p>Il a décidé d'aller voir son médecin des peluches, le nounoursologue.</p>
+            <p>Le nounoursologue lui a dit :</p>
+            <blockquote class="medecin">
+                <p>&laquo;C'est <span class="rose">la blastouille</span>.</p>
+                <p>C'est une maladie que seuls les ours en peluche peuvent attraper ! Quand tu as mangé hier, tu as aussi avalé <span class="vert">un microbe</span>.</p>
+                <p>D'après tes symptômes, je sais que c'est une bactérie qui t'a rendu malade, <span class="orange">mais je ne sais pas laquelle précisément</span>.&raquo;</p>
+            </blockquote>
+            <p>Le nounoursologue lui a donné <span class="jaune">des médicaments</span>.</p>
+            <blockquote><p>&laquo;Voilà tes médicaments Nounours, reviens me voir si tu ne vas pas mieux !&raquo;</p></blockquote>
+            <p>Une semaine après les médicaments n’ont toujours pas marché ! Il retourne chez le nounoursologue…</p>
+            <blockquote><p>&laquo;Il faut faire appel <span class="violet">aux experts bioinfo</span> !&raquo;</p></blockquote>
+            <p>Aide-nous à mener l'enquête pour identifier la bactérie responsable de la maladie de Nounours et lui trouver un médicament.</p>
+        </div>
 
         <p class="text-center"><a class="btn btn-outline-primary btn-lg bouton-enquete" role="button" href= "./fiche_enquete.php">Commencer l'enquête</a></p>
     </div>
 
-    <div class="col-4">
-        <p><img src="../media/nounours/photo_nounours.png" width="100%" class="round" /></p>
+    <div class="col-3 justify-content-center">
+        <div class="photoNounours">
+            <img src="../media/nounours/photo_nounours.png" width="100%"/>
+            <p class="legende">Nounours</p>
+        </div>
     </div>
 </div>
 
+<!--
 <div class="row">
     <div class="card col-4">
         <img src="../media/nounours/nounours_masque.JPG" class="img-fluid"/>
@@ -38,6 +56,7 @@ require("headerNounours.php");
         <p class="card-text"></p>
     </div>
 </div>
+-->
 
 <?php
 require("footerNounours.php");
