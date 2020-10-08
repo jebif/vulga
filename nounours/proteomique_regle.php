c626d48b071cc2ec4b8fc5ad38a067af76a4c010
marsu15 <julien.fumey@gmail.com>
2020-10-08 12:14:26 +0200

    Change in prot size

--- nounours/proteomique_regle.php
+++ nounours/proteomique_regle.php
@@ -6,42 +6,42 @@ require("headerNounours.php");
 <h1>Quelle est la structure de la protéine qui empêche les antibiotiques d'agir ?</h1>
 
 <div class="row">
-    <div class="col-3">
+    <div class="col-8">
         <div class="explication">
             <h2>Sélectionnes les protéines vérifiant les règles</h2>
             <p>Tu peux naviguer entre les règles grâce aux flèches à gauche et à droite.</p>
             <div id="carouselRegles" class="carousel slide" data-interval="false">
                 <div class="carousel-inner">
                     <div class="carousel-item active" id="coude">
-                        <img src="../media/nounours/proteomique/coude.png" class="d-block w-100" />
+                        <img src="../media/nounours/proteomique/coude.png" class="d-block w-25" />
                         <div class="carousel-caption">
                             <h5>Le coude</h5>
                             <p>3 acides aminés bleus suivis de 3 verts forment un coude.</p>
                         </div>
                     </div>
                     <div class="carousel-item" id="membrane">
-                        <img src="../media/nounours/proteomique/membrane.png" class="d-block w-100" />
+                        <img src="../media/nounours/proteomique/membrane.png" class="d-block w-25" />
                         <div class="carousel-caption">
                             <h5>La membrane</h5>
                             <p>À proximité d’une paroi jaune, les acides aminés cyan entre en contact avec celle-ci.</p>
                         </div>
                     </div>
                     <div class="carousel-item" id="no-contact">
-                        <img src="../media/nounours/proteomique/nocontact.png" class="d-block w-100" />
+                        <img src="../media/nounours/proteomique/nocontact.png" class="d-block w-25" />
                         <div class="carousel-caption">
                             <h5>Le sans-contact</h5>
                             <p>Les acides aminés bleu et cyan ne sont jamais en contact.</p>
                         </div>
                     </div>
                     <div class="carousel-item" id="contact">
-                        <img src="../media/nounours/proteomique/contact.png" class="d-block w-100" />
+                        <img src="../media/nounours/proteomique/contact.png" class="d-block w-25" />
                         <div class="carousel-caption">
                             <h5>Le contact</h5>
                             <p>Les acides aminés bordeaux séparés par 2 à 6 autres acides aminés sont en contact.</p>
                         </div>
                     </div>
                     <div class="carousel-item" id="demitour">
-                        <img src="../media/nounours/proteomique/demitour.png" class="d-block w-100" />
+                        <img src="../media/nounours/proteomique/demitour.png" class="d-block w-25" />
                         <div class="carousel-caption">
                             <h5>Le demi-tour</h5>
                             <p>2 acides aminés bleus précédés et suivis par 2 verts forment un demi-tour.</p>
@@ -63,7 +63,7 @@ require("headerNounours.php");
     <?php
     if(isset($_GET['r']) && $_GET['r'] == "success") {
     ?>
-    <div class="col-8">
+    <div class="col-4">
         <div class="explication">
             <div class="alert alert-success" role="alert">Bravo&nbsp;! Tu as trouvé que la protéine 1 rend Nonours malade. C'est la seule à respecter toutes les règles.</div>
             <img src="../media/nounours/proteomique/prot-2.png" width="50%" />
@@ -74,8 +74,10 @@ require("headerNounours.php");
     <?php
     }else if(! isset($_GET['r']) || isset($_GET['r']) && $_GET['r'] == "coude"){
     ?>
+
         <div class="col-4">
             <div class="explication">
+                <div role="alert" id="alertDiv" style="display:none;"></div>
                 <p>Afin d'identifier la bonne protéine, tu peux naviguer dans les règles à gauche puis cliques sut toutes les protéines ci-dessous qui ne la respectent pour les éliminer. Lorsque tu as éliminé une protéine, elle apparaît sur fond rouge. Ne recliques pas dessus&nbsp;!</p>
                 <p>Une fois que tu as éliminé toutes les protéines qui ne respectent pas l'ensemble des règles, cliques sur le bouton Vérifier en bas de la page.</p>
             </div>
@@ -83,7 +85,6 @@ require("headerNounours.php");
 </div>
 
 <div class="row">
-    <div role="alert" id="alertDiv" style="display:none;"></div>
     <div class="col-4">
         <button class="list-group-item" onclick="clickOnProt(this, getActiveCarouselItem())" value="1">
             <img src="../media/nounours/proteomique/prot-2.png"  class="d-block w-100 img-fluid" />
